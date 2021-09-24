23de60afef6ae6b1c7da61623df5f791c3364955
Christian Bravo <C_CBravo@medicalcardsystem.com>
2021-09-24 09:06:37 -0500

    WS: Added cors policy

--- Survey.WebService/Startup.cs
+++ Survey.WebService/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjecton;
 using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
 
 namespace Survey.WebService
 {
@@ -14,6 +16,7 @@ namespace Survey.WebService
             Configuration = configuration;
         }
 
+        private readonly string defaultCorsPolicy = "defaultPolicy";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -24,6 +27,17 @@ namespace Survey.WebService
                 .AddDataServices()
                 .AddDomainServices()
                 .AddControllers();
+
+            services.AddCors(ConfigureCors);
+        }
+
+        public void ConfigureCors(CorsOptions cors)
+        {
+            List<string> domainsAllowed = Configuration.GetSection("CorsAllowedDomains").Get<List<string>>();
+            cors.AddPolicy(defaultCorsPolicy, builder =>
+            {
+                domainsAllowed.ForEach(domain => builder.WithOrigins(domain).AllowAnyMethod().AllowAnyHeader());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,13 +47,10 @@ namespace Survey.WebService
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
+            app.UseCors(defaultCorsPolicy);
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
